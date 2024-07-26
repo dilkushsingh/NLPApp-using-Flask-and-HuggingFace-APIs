682f982535c118b4aa27bbf6c1bd1e31bc785ed8
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-07-26 12:35:54 +0530

    create the final flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from mydb import Database
+from myapi import API
 
 app = Flask(__name__)
 dbo = Database()
+apio = API()
 @app.route('/')
 def index():
     return render_template('login.html')
@@ -43,12 +45,35 @@ def ner():
 @app.route('/perform_ner', methods=['post'])
 def perform_ner():
     text = request.form.get('text')
+    result = apio.ner(text)
+    txt = ""
+    for i in range(len(result)):
+        txt += f"'entity_group':'{result[i]['entity_group']}', 'word':'{result[i]['word']}', 'score':{round(result[i]['score'] * 100, 2)}% \n"
+    return render_template('ner.html', result=txt)
 
 @app.route('/sentiment')
 def sentiment():
     return render_template('sentiment.html')
 
+@app.route('/perform_sentiment', methods=['post'])
+def perform_sentiment():
+    text = request.form.get('text')
+    result = apio.sentiment_analysis(text)
+    print(result)
+    txt = ''
+    for i in range(len(result[0])):
+        txt += f'{result[0][i]['label']} : {round(result[0][i]['score'] * 100, 2)}%\n'
+    return render_template('sentiment.html', result=txt)
+
+
 @app.route('/emotion')
 def emotion():
     return render_template('emotion.html')
+
+@app.route('/perform_emotion', methods=['post'])
+def perform_emotion():
+    text = request.form.get('text')
+    response = apio.emotion(text)
+    return render_template('emotion.html', result=response)
+
 app.run(debug=True)
---
+++ myapi.py
@@ -0,0 +1,21 @@
+import requests
+class API:
+    def __init__(self):
+        self.api = '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'
+        self.headers = {"Authorization": f"Bearer {self.api}"}
+    def ner(self, text):
+        API_URL = "https://api-inference.huggingface.co/models/dbmdz/bert-large-cased-finetuned-conll03-english"
+        payload = {"inputs": text}
+        response = requests.post(API_URL, headers=self.headers, json=payload)
+        return response.json()
+    def sentiment_analysis(self, text):
+        API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+        payload = {"inputs": text}
+        response = requests.post(API_URL, headers=self.headers, json=payload)
+        return response.json()
+    def emotion(self, text):
+        API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
+        payload = {'inputs': text}
+        response = requests.post(API_URL, headers=self.headers, json=payload)
+        return response.json()
+
